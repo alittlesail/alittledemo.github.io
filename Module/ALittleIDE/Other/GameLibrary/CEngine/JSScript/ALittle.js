33d118e357d576ea295b7abf253eed84da09614f
xingfanou <757609414@qq.com>
2020-04-27 14:54:15 +0800

    添加canvas支持

--- Module/ALittleIDE/Other/GameLibrary/CEngine/JSScript/ALittle.js
+++ Module/ALittleIDE/Other/GameLibrary/CEngine/JSScript/ALittle.js
@@ -5,7 +5,11 @@ __ALITTLEAPI_CEngine_Init = function(base_path) {
 	return new Promise(async function(___COROUTINE, ___) {
 		await Require(base_path + "CEngine/UISystem/IShow");
 		{
-			await Require(base_path + "../../JSNative/pixi");
+			if (!PIXI.utils.isWebGLSupported()) {
+				await Require(base_path + "../../JSNative/pixi-legacy.min");
+			} else {
+				await Require(base_path + "../../JSNative/pixi.min");
+			}
 			await Require(base_path + "../../JSNative/pixi-textinput");
 			await Require(base_path + "Adapter/PIXI/JSystem");
 			await Require(base_path + "Adapter/PIXI/JShow");
