faa8b2f4b9af66ce9c8142969be6d018e47753d0
xingfanou <757609414@qq.com>
2020-09-08 13:43:05 +0800

    优化

--- Module/AUIPlugin/JSScript/Main.js
+++ Module/AUIPlugin/JSScript/Main.js
@@ -28,9 +28,14 @@ AUIPlugin.__Plugin_Setup = async function(control, module_base_path, script_base
 	AUIPlugin.g_Control = control;
 	AUIPlugin.g_ModuleBasePath = module_base_path;
 	AUIPlugin.g_ScriptBasePath = script_base_path;
-	await RequireFromPaths(script_base_path, "AUI/", 
+	await RequireFromPaths(script_base_path, "AUI/", ["AUIWebLoginManager.alittle", "AUIVersionManager.alittle", "AUITool.alittle"
+		, "AUIRightMenu.alittle", "AUIImageCutPlugin.alittle", "AUIIMEManager.alittle"
 		, "AUIFileSelectLayout.alittle", "AUIFileSelectDialog.alittle", "AUIEditImageDialog.alittle"]);
-	await RequireFromPaths(script_base_path, "AUICodeEdit/", 
+	await RequireFromPaths(script_base_path, "AUICodeEdit/", ["AUICodeSelectCursor.alittle", "AUICodeRevocation.alittle", "AUICodeProject.alittle"
+		, "AUICodeParamList.alittle", "AUICodeLineNumber.alittle", "AUICodeLineContainer.alittle"
+		, "AUICodeLanguage.alittle", "AUICodeFilterScreen.alittle", "AUICodeEdit.alittle"
+		, "AUICodeDefine.alittle", "AUICodeCursor.alittle", "AUICodeComponent.alittle"
+		, "AUICodeCompleteScreen.alittle", "ABnf/AUICodeCommon.alittle", "ABnf/AUICodeALittleScript.alittle"
 		, "ABnf/AUICodeABnf.alittle"]);
 	AUIPlugin.g_AUIIMEManager.Setup();
 }
